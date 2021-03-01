a71be635a8eac50dc9b370db90e138d1b2d7b5a9
Sarah Ferrolino <sferrolino@ryerson.ca>
2021-03-01 01:51:29 -0500

    Update Dataquest_W1.py

--- Dataquest_W1.py
+++ Dataquest_W1.py
@@ -9,45 +9,36 @@ import pandas as pd
 
 #reading csv file
 dc_listings = pd.read_csv ('dc_airbnb.csv')
-#print first row of .csv file we read in dc_listings
-print(dc_listings[:1])
+#print first 4 columns (1-4), OMIT index 0 b/c it's the header
+print(dc_listings[1:5])
+#print first 10 entries of price column
+print(dc_listings[:10]['price'])
 
-#initialize
-our_value = 3
 #df.iloc[r][c] - allows us to select a particular cell in the data set
 first_living_space_value = dc_listings.iloc[0]['accommodates']
 #np.abs(v2 - v1) - Calculating the absolute value of a number
 first_distance = np.abs(first_living_space_value - our_value)
 
-#get euclidean distance
-new_listing = 3
 #df[column].apply(lambda x: np.abs(x)) - 
 dc_listings['distance'] = dc_listings['accommodates'].apply(lambda x: np.abs(x - new_listing))
-#display unique values from distance column
+#df[column].value_counts() - display unique values from column
 print(dc_listings['distance'].value_counts())
 
-
-#
 #initalize shuffled_index
 shuffled_index = np.random.permutation(len(dc_listings["distance"]))
 
-#use loc[row_indexer, column_indexer] on dc_listings
+#use loc[row_indexer, column_indexer] 
 dc_listings = dc_listings.loc[shuffled_index]
 
-#sort by distance column
+#df.sort_values(by=[column]) - sorting rows by column
 dc_listings = dc_listings.sort_values(by=['distance'])
 
-#print first 10
-print(dc_listings[:10]['price'])
-
-#
-#remove commas and dollar sign char
+#df[column].str.replace(char to replace, replacement) and list.str.replace(char to replace, replacement) - remove commas and dollar sign char
 stripped_commas = dc_listings['price'].str.replace(',', '')
 stripped_dollars = stripped_commas.str.replace('$', '')
 
-#change to float datatype
+#list.astype(datatype) - change datatype
 dc_listings['price'] = stripped_dollars.astype(float)
 
 #calculate mean value (average of numbers)
 mean_price = dc_listings[0:5]['price'].mean()
-print(mean_price)
