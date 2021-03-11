aa5da03c3b9d0fd81fe3d19b518db6e1ba9bd804
Sarah Ferrolino <sferrolino@ryerson.ca>
2021-03-11 02:25:27 -0500

    Create Dataquest_W4.py

---
+++ Dataquest_W4.py
@@ -0,0 +1,7 @@
+#Dataquest Week 4 Commands - Intermediate Stream
+
+#### Mission 1: Logistic Regression ####
+
+#### Mission 2: Introduction to evaluating binary classifiers ####
+
+#### Mission 3: Multiclass Classification ####
