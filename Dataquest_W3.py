1e41d6267c9aa3893cabc4f06ad58abfbf1a4c00
Sarah Ferrolino <sferrolino@ryerson.ca>
2021-03-11 02:26:22 -0500

    Update Dataquest_W3.py

--- Dataquest_W3.py
+++ Dataquest_W3.py
@@ -1,1 +1,11 @@
 #Dataquest Week 3 Commands - Intermediate Stream
+
+### Mission 1: Linear Regression Model ###
+
+### Mission 2: Feature Selection ###
+
+### Mission 3: Gradient Descent ###
+
+### Mission 4: Ordinary Least Squares ###
+
+### Mission 5: Processing and Transforming Features###
